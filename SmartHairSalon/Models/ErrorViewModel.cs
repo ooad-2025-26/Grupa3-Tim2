e30dab96bd7d71410959587b0b6ffb44d54051a1
akantic2 <akantic2@etf.unsa.ba>
2026-04-28 19:16:28 +0200

    Task 6

---
+++ SmartHairSalon/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHairSalon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
